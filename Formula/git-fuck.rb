6e2ea9fa61ddf04a3b39784bb8ead7148f96ff53
Max Harley <augeocacher@gmail.com>
2018-07-16 19:19:43 -0400

    Updated to fix PR

--- Formula/git-fuck.rb
+++ Formula/git-fuck.rb
@@ -4,8 +4,6 @@ class GitFuck < Formula
   url "https://github.com/t94j0/git-fuck/archive/1.0.tar.gz"
   sha256 "fa5011ea18b46dfbbf0ec15a2142599dcc3a736dd04e3d9d97b09b5631c4196c"
 
-  depends_on "git" => :run
-
   def install
 	  bin.install "git-fuck"
   end
